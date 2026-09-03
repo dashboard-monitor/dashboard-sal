b518cd4fb411dc1a055b0818f5cdada9a1620681
dashboard-monitor <henry.palladino@hotmail.it>
2026-09-03 20:37:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if check_password():
         SHEET_ID = "12gik-EYKeVeJvOpohkPM-nUVJLbiDkKpI-XT9Mx2RAA"
         
         # Link di esportazione nativa che forza Google a inviare un file Excel (.xlsx) reale in background
-        url_diretto = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=xlsx"
+        url_diretto = f"https://google.com{SHEET_ID}/export?format=xlsx"
         
         # Lettura iniziale della struttura dei fogli di lavoro dal cloud
         excel_file = pd.ExcelFile(url_diretto, engine='openpyxl')
@@ -109,6 +109,11 @@ if check_password():
                 
                 df_plot[col_percentuali] = df_plot[col_percentuali].astype(str).str.replace('%', '', regex=False)
                 df_plot[col_percentuali] = pd.to_numeric(df_plot[col_percentuali], errors='coerce')
+                
+                # Sotto viene moltiplicato il valore decimale per 100 (es. 1.0 -> 100.0)
+                if "%" in col_percentuali.lower() or "completamento" in col_percentuali.lower() or "sal" in col_percentuali.lower():
+                    df_plot[col_percentuali] = df_plot[col_percentuali] * 100
+                
                 df_plot = df_plot.dropna(subset=[col_percentuali])
                 df_plot = df_plot.sort_values(by=col_percentuali, ascending=True)
                 
